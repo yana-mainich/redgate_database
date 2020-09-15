6da75db9053eb33e51dbafd4afdadfd50046e6f8
YMainich <ymainich@nps.org.au>
2020-09-15 18:28:05 +1000

    Reverted the changes in the Widgets table

--- Tables/dbo.Widgets.sql
+++ Tables/dbo.Widgets.sql
@@ -2,8 +2,7 @@ CREATE TABLE [dbo].[Widgets]
 (
 [RecordID] [int] NOT NULL IDENTITY(1, 1),
 [Description] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[SKU] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Rating] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[SKU] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Widgets] ADD CONSTRAINT [PK_Widgets] PRIMARY KEY NONCLUSTERED  ([RecordID]) ON [PRIMARY]
